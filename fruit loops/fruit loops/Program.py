49c2feed6cb1d22bce49a0ac06b50edf97481bdd
Hunter Brotzman <brotzman.h@stu.janesville.k12.wi.us>
2024-10-22 09:22:35 -0500

    10/22

---
+++ fruit loops/fruit loops/Program.py
@@ -0,0 +1,15 @@
+﻿colors = ["red", "YELLOW", "blue"]
+print(colors)
+print(colors[1])
+
+fruits = ["MINONS", "TONIGHT", "WE", "STELL", "THE", "MOON"]
+for item in fruits:
+    print(item)
+for index in range (len(fruits)):
+    print(fruits[index])
+print("")
+
+# lastfruit = fruits[lan(fruits)-1]
+lastfruit = fruits[-1]
+print(lastfruit)
+input()
