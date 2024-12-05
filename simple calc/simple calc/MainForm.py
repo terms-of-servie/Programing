39aca2ec4982109101fdc7f2e9d75347562d5532
Hunter Brotzman <brotzman.h@stu.janesville.k12.wi.us>
2024-12-05 09:03:39 -0600

    e

---
+++ simple calc/simple calc/MainForm.py
@@ -0,0 +1,20 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self.SuspendLayout()
+        # 
+        # MainForm
+        # 
+        self.ClientSize = System.Drawing.Size(946, 450)
+        self.Name = "MainForm"
+        self.Text = "simple calc"
+        self.ResumeLayout(False)
+
